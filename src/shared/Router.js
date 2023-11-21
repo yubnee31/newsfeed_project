c79bfb2456d7fa0815d77d2f774f8a2fde633c5b
yubnee31 <tydbqls@gmail.com>
2023-11-21 12:29:56 +0900

    commit1

---
+++ src/shared/Router.js
@@ -0,0 +1,20 @@
+import Detail from 'pages/Detail';
+import Home from 'pages/Home';
+import Mypage from 'pages/Mypage';
+import React from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="detail" element={<Detail />} />
+        <Route path="mypage" element={<Mypage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
