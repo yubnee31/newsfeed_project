127a440d9643f0a4e1bfc88a1a78b59547c6ab74
안준표 <dkswnsvy2010@naver.com>
2023-11-24 16:16:31 +0900

    중간커밋

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -1,82 +1,204 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import { addDoc, collection} from "firebase/firestore";
-import { db } from '../firebase';
-import { auth, storage } from '../firebase';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
+import styled from 'styled-components';
+import { addDoc, collection } from 'firebase/firestore';
+import { db, storage } from '../firebase';
 import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
-import defaultItem from "assets/defaultItem.png"
+import { useNavigate } from 'react-router-dom';
+import defaultItem from 'assets/defaultItem.png';
 
-export default function AddForm({items, setItems}) {
-    const navigate = useNavigate();
-    const [selectedFile, setSelectedFile] = useState();
-    const [itemImg, setitemImg] = useState(defaultItem);
+export default function AddForm({ items, setItems }) {
+  const navigate = useNavigate();
+  const user = JSON.parse(localStorage.getItem('login user'));
+  const [itemInfo, setItemInfo] = useState('');
+  const [itemPrice, setItemPrice] = useState('');
+  const [itemTitle, setItemTitle] = useState('');
+  const [images, setImages] = useState({
+    imageFiles: [],
+    previewURLs: Array.from({ length: 4 }, (_, index) => `img/default_image${index + 1}.png`),
+  });
+  const inputRef = useRef(null);
 
-    const user = JSON.parse(localStorage.getItem('login user'));
-    const [itemInfo, setItemInfo] = useState("");
-    const [itemPrice, setItemPrice] = useState("");
-    const [itemTitle, setItemTitle] = useState("");
-    const [userId, setUserId] = useState("");
+  useEffect((event) => {
+    event.preventDefault();
+    setImages({
+      imageFiles: [],
+      previewURLs: Array.from({ length: 4 }, (_, index) => `img/default_image${index + 1}.png`),
+    });
+  }, []);
 
-    const handleFileSelect = (event)=>{
-        setSelectedFile(event.target.files[0]);
+  const handleFileChange = async (e) => {
+    const selectedFiles = Array.from(e.target.files).slice(0, 4);
+
+    try {
+      const uploadPromises = selectedFiles.map(async (file) => {
+        const storageRef = ref(storage, `images/${file.name}`);
+        await uploadBytes(storageRef, file);
+        return await getDownloadURL(storageRef);
+      });
+
+      const downloadURLs = await Promise.all(uploadPromises);
+
+      setImages((prevImages) => ({
+        ...prevImages,
+        imageFiles: selectedFiles,
+        previewURLs: downloadURLs,
+      }));
+    } catch (error) {
+      console.error('이미지 업로드 에러 ', error);
     }
-    const handleUpload = async() =>{
-        const imageRef = ref(
-            storage,
-            `${user.uid}/item/${selectedFile.name}`
-        );
-        await uploadBytes(imageRef, selectedFile);
-        const downloadURL = await getDownloadURL(imageRef);
-        setitemImg(downloadURL);
+  };
 
-    };
-   // 이걸 이미지랑 엮어서 등록하기에 같이 메인페이지에 보내져야함 
-    const addItem = async (event)=>{
-        event.preventDefault();
-        const newItem = {itemInfo, itemPrice, itemTitle, sold:false, userId:user.uid};
-        setItems((prev)=>{
-            return [...items, newItem];
-        })
-        const collectionRef = collection(db, "items");
-        await addDoc(collectionRef, newItem);
-        
-        setItemInfo("");
-        setItemPrice("");
-        setItemTitle("");
-        setUserId("");
-        navigate('/mypage')
+  const deleteImages = () => {
+    setImages((prevImages) => ({
+      ...prevImages,
+      imageFiles: [],
+      previewURLs: Array.from({ length: 4 }, (_, index) => `img/default_image${index + 1}.png`),
+    }));
+  };
+
+  const sendImagesToServer = () => {
+    if (images.imageFiles.length > 0) {
+      alert('서버에 등록이 완료되었습니다!');
+
+      setImages((prevImages) => ({
+        ...prevImages,
+        imageFiles: [],
+        previewURLs: Array.from({ length: 4 }, (_, index) => `img/default_image${index + 1}.png`),
+      }));
+    } else {
+      alert('사진을 등록하세요!');
     }
+  };
 
+  const addItem = async () => {
+    const newItem = { itemInfo, itemPrice, itemTitle, sold: false, userId: user.uid };
+    setItems((prev) => [...items, newItem]);
+    const collectionRef = collection(db, 'items');
+    await addDoc(collectionRef, newItem);
 
-
+    setItemInfo('');
+    setItemPrice('');
+    setItemTitle('');
+    navigate('/mypage');
+  };
 
   return (
     <AddSection>
-        <form onSubmit={addItem} key = {user.uid}>
-            <div>
-            <img src = {itemImg}/>
-            <br />
-            <input type = "file"  onClick={handleFileSelect}/>
-            <br/>
-            <button type = "button" onClick={handleUpload}>Upload</button>
-            <br/>
-            <input value = {itemInfo} onChange={(event)=>setItemInfo(event.target.value)} placeholder='상품 설명'/>
-            <br />
-            <input value = {itemPrice} onChange={(event)=>setItemPrice(event.target.value)} placeholder='가격'/>
-            <br />
+         <div>
 
-            <input value = {itemTitle} onChange={(event)=>setItemTitle(event.target.value)} placeholder='제목'/>
-            <br />
-            <br />
+<div>
+  {images.previewURLs.map((url, index) => (
+    <img key={index} src={url} alt={`Preview ${index + 1}`} />
+  ))}
+</div>
+</div>
+      <form onSubmit={addItem} key={user.uid}>
+        <StyledContainer>
+            <FileUploadSection>
+          <UploaderWrapper>
+            <input
+              type="file"
+              accept="image/*"
+              onChange={handleFileChange}
+              onClick={(e) => (e.target.value = null)}
+              ref={inputRef}
+              multiple="multiple"
+            />
+            
+            <UploadButton>
+              <CustomButton
+                type="primary"
+                variant="contained"
+                onClick={() => inputRef.current.click()}
+              >
+                Preview
+              </CustomButton>
+              <CustomButton color="error" variant="contained" onClick={deleteImages}>
+                Delete
+              </CustomButton>
+              <CustomButton
+                color="success"
+                variant="contained"
+                onClick={sendImagesToServer}
+              >
+                Upload
+              </CustomButton>
+            </UploadButton>
+          </UploaderWrapper>
 
-            <button onClick = {addItem}>완료</button>
-            </div>
-        </form>
+          <InputField
+            value={itemInfo}
+            onChange={(event) => setItemInfo(event.target.value)}
+            placeholder="상품 설명"
+          />
+          <br />
+          <InputField
+            value={itemPrice}
+            onChange={(event) => setItemPrice(event.target.value)}
+            placeholder="가격"
+          />
+          <br />
+          <InputField
+            value={itemTitle}
+            onChange={(event) => setItemTitle(event.target.value)}
+            placeholder="제목"
+          />
+          <br />
+          <br />
+          <SubmitButton type={"onSubmit"} onClick={addItem}>완료</SubmitButton>
+          </FileUploadSection>
+        </StyledContainer>
+      </form>
     </AddSection>
-  )
+  );
 }
 
-const AddSection = styled.section`
+const StyledContainer = styled.div`
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+const FileUploadSection = styled.div`
+    width: 50%;
+`
+const UploaderWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+  padding: 20px;
+`;
 
+const UploadButton = styled.div`
+  display: flex;
+  gap: 8px;
+`;
+
+const CustomButton = styled.button`
+  width: 100px;
+`;
+
+const InputField = styled.input`
+  width: 80%;
+  padding: 8px;
+  margin-bottom: 16px;
+`;
+
+const SubmitButton = styled.button`
+  padding: 10px;
+  background-color: #4caf50;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #45a049;
+  }
+`;
+
+const AddSection = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
--- src/pages/Addpage.jsx
+++ src/pages/Addpage.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Imageadd from 'components/Imageadd';
 import Layout from 'components/layouts/Layout';
 import AddForm from 'components/AddForm';
 
@@ -9,7 +8,6 @@ import AddForm from 'components/AddForm';
 function Addpage() {
     return (
         <Layout>
-            <Imageadd/>
             <AddForm/>
         </Layout>
     ) 
