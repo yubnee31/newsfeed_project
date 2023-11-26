7710a45d8e0140c4bcc9f3a4a0037569efb74c95
yubnee31 <tydbqls@gmail.com>
2023-11-27 00:42:00 +0900

    수정4

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -9,6 +9,7 @@ export default function Edit({ items, setItems }) {
   const params = useParams();
   const navigate = useNavigate();
   const selectedItem = items.find((item) => item.id === params.id);
+  console.log(selectedItem);
   const [editMode, setEditMode] = useState(false);
   const [changedTitle, setChangedTitle] = useState('');
   const [changedInfo, setChangedInfo] = useState('');
