4c5e9f9f034c42de008ec246bb1668d329f7a1f2
Solyi Park <itsoholy21@gmail.com>
2023-11-26 11:54:22 +0900

    검색 기능 로직추가 / UI 미완

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,8 +1,9 @@
 import SignUpLogIn from 'components/SignUpLogIn';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router';
 import Category from 'components/Category';
+import { useItems } from 'shared/Items';
 
 const SearchDiv = styled.div`
   display: flex;
@@ -39,26 +40,26 @@ const SearchInput = styled.input`
   border: 3px solid #ab7323;
   border-radius: 20px;
   font-size: 20px;
-  /* position: relative; */
   /* margin-top: 15px; */ // 대신 SearchDiv의 margin-top을 20->35로 조정
 `;
 
 const SearchBtn = styled.button`
   display: flex;
   margin-left: 20px;
-  /* margin-top: 15px; */ //대신 SearchDiv의 margin-top을 20->35로 조정
+  /* margin-top: 15px; */ //솔, SearchDiv의 margin-top을 20->35로 조정
   position: relative;
-  width: 60px;
+  width: 100px; //솔, 너비 60 -> 100
   height: 60px;
   align-items: center;
   border: 3px solid #ab722374;
   background-color: transparent;
   border-radius: 15px;
   justify-content: center;
-  font-size: 15px;
+  font-size: 18px; // 15 -> 18
+  font-weight: 600;
   color: #ab7323;
   &:hover {
-    cursor: pointer; //hover시 커서 변경
+    cursor: pointer; //솔, hover시 커서 변경
   }
 `;
 
@@ -75,9 +76,9 @@ const Title = styled.h1`
 
 const PostBtn = styled.button`
   display: flex;
-  margin-left: 20px;
-  /* margin-top: 15px; */ //대신 SearchDiv의 margin-top을 20->35로 조정
-  width: 100px;
+  margin: 13px 0 0 20px;
+  /* margin-top: 15px; */ //솔, SearchDiv의 margin-top을 20->35로 조정
+  width: 200px;
   height: 60px;
   align-items: center;
   border: none;
@@ -88,13 +89,43 @@ const PostBtn = styled.button`
   font-weight: 600;
   color: white;
   &:hover {
-    cursor: pointer; //hover시 커서 변경
+    cursor: pointer; //솔, hover시 커서 변경
     background-color: #ae7a32c7;
   }
 `;
 
+const SearchForm = styled.form`
+  // 솔, SearchDiv -> SearchForm
+  display: flex;
+  align-items: center;
+  justify-content: flex-start; // 솔, 카테고리 버튼을 왼쪽 끝으로 몰기 위해 flex-start 추가
+  /* background-color: red; */
+  width: 1200px; // 솔, 상품리스트 width와 맞춤 동일하게 설정
+  height: 50px;
+  margin: 35px auto 20px auto; // 솔, 20px auto 에서 수정
+`;
+
 function Header() {
   const navigate = useNavigate();
+  const [items, setItems] = useItems();
+
+  const [searchInput, setSearchInput] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
+  const searchInputHandler = (e) => setSearchInput(e.target.value);
+  const search = (e) => {
+    e.preventDefault();
+    const filtered = items.filter((item) => {
+      return item.itemTitle.includes(searchInput) || item.itemInfo.includes(searchInput);
+    });
+    setSearchResults(filtered);
+  };
+  useEffect(() => {
+    console.log('===========================');
+    console.log('검색결과', searchResults);
+    console.log('===========================');
+  }, [searchResults]);
+
   return (
     <>
       <Title>GAMZA</Title>
@@ -104,8 +135,14 @@ function Header() {
         <HomeBtn type="button" onClick={() => navigate('/')}>
           <Img src="https://img.freepik.com/premium-vector/potato-root-vegetables-carbohydrate-agriculture-farm-product_22052-4629.jpg"></Img>
         </HomeBtn>
-        <SearchInput placeholder=" 검색어를 입력하세요"></SearchInput>
-        <SearchBtn>검색</SearchBtn>
+        <SearchForm onSubmit={search}>
+          <SearchInput
+            value={searchInput}
+            onChange={(e) => searchInputHandler(e)}
+            placeholder=" 검색어를 입력하세요."
+          ></SearchInput>
+          <SearchBtn type="submit">검색</SearchBtn>
+        </SearchForm>
         {/* 게시물 작성 버튼 연결 */}
         <PostBtn onClick={() => navigate('/AddPage')}>상품등록</PostBtn>
       </SearchDiv>
