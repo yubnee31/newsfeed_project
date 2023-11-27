55c91c41e15ef45f43e26e44de8c8baf113c31b7
Solyi Park <itsoholy21@gmail.com>
2023-11-27 10:52:05 +0900

    홈화면에서 카테고리 분류기능 구현

--- src/components/layouts/Layout.jsx
+++ src/components/layouts/Layout.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import Header from './Header';
 function Layout(props) {
+  console.log(props);
   return (
     <div>
-      <Header />
+      <Header selectedCategory={props.selectedCategory} setSelectedCategory={props.setSelectedCategory} />
       <main>{props.children}</main>
     </div>
   );
